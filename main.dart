c31b31cf390ea50f3541a19dac8fa0a86b79cd5c
phamtanphat <phatdroid94@gmail.com>
2024-03-23 18:41:19 +0700

    interpolation

--- main.dart
+++ main.dart
@@ -2,13 +2,26 @@ import 'dart:ffi';
 
 void main() {
   // 1: Khai bao bien va kieu du lieu
-  String name = "";
-  int age = 30;
-  double weight = 75.5;
-  num height = 175.5;
-  bool isMale = true;
-  String unicode = "\u{00F8}";
-  Runes unicodeMultiCharacter = Runes("\u{00F8}\u{00F9}\u{00FA}");
-  print(unicode);
-  print(String.fromCharCodes(unicodeMultiCharacter));
+  // String name = "";
+  // int age = 30;
+  // double weight = 75.5;
+  // num height = 175.5;
+  // bool isMale = true;
+  // String unicode = "\u{00F8}";
+  // Runes unicodeMultiCharacter = Runes("\u{00F8}\u{00F9}\u{00FA}");
+  // print(unicode);
+  // print(String.fromCharCodes(unicodeMultiCharacter));
+
+  // var name = "";
+  // dynamic value = "Hello";
+  // value = 1;
+  // print(name.runtimeType);
+  // print(value.runtimeType);
+
+  // 2: Interpolation
+  var name = "Phat";
+  var age = 30;
+  var output = "$name$age";
+
+  print(output);
 }
