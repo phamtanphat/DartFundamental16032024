c4687e887debe8eeacbd7561c38a3ab3299eaf85
phamtanphat <phatdroid94@gmail.com>
2024-03-23 19:24:31 +0700

    ++ --

--- main.dart
+++ main.dart
@@ -19,9 +19,41 @@ void main() {
   // print(value.runtimeType);
 
   // 2: Interpolation
-  var name = "Phat";
-  var age = 30;
-  var output = "$name$age";
+  // var name = "Phat";
+  // var age = 30;
+  // var output = "$name$age";
+  //
+  // print(output);
 
-  print(output);
+  // 3: Toan tu
+  // var a = 5;
+  // var b = 3;
+  // var result = a % b;
+  // print(result);
+
+  // String? value = null;
+  // print(value ?? "Default value");
+
+  // Toan tu ++
+  // Case 1 (a++)
+  // Gan gia tri cua a
+  // Thuc hien bieu thuc a = a + 1
+
+  // Case 2 (++a)
+  // Thuc hien bieu thuc a = a + 1
+  // Gan gia tri cua a
+  var a = 5;
+  var b = 7;
+  var result = --a + b-- + b-- + ++a + ++b - a--;
+  // 4 + b-- + b-- + ++a + ++b - a--; a = 4, b = 7
+  // 4 + 7 + b-- + ++a + ++b - a--; a = 4, b = 6
+  // 4 + 7 + 6 + ++a + ++b - a--; a = 4, b = 5
+  // 4 + 7 + 6 + 5 + ++b - a--; a = 5, b = 5
+  // 4 + 7 + 6 + 5 + 6 - a--; a = 5, b = 6
+  // 4 + 7 + 6 + 5 + 6 - 5; a = 4, b = 6
+
+  // a = 4, b = 6, result = 23
+  print("A: $a");
+  print("B: $b");
+  print("Result: $result");
 }
